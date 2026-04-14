e2e0f047cf228aa2d46f150ba5c3febc01ca1fe4
Omelianchuk Oleksandr <89081142+Omelyanchuk04@users.noreply.github.com>
2026-04-15 00:19:34 +0300

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,8 @@
-"use client";
-
-import { useEffect } from "react";
 import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
 import About from "@/components/sections/About/About";
 import Services from "@/components/sections/Services/Services";
 import Projects from "@/components/sections/Projects/Projects";
 export default function Home() {
-  // 🚨 ЖУЧОК ДЛЯ ПОШУКУ СКРОЛУ
-  useEffect(() => {
-    const findScroller = (e) => {
-      console.log("🔥 СКРОЛИТЬСЯ ЕЛЕМЕНТ:", e.target);
-    };
-
-    // Трекаємо всі скроли на сторінці
-    window.addEventListener("scroll", findScroller, true);
-    return () => window.removeEventListener("scroll", findScroller, true);
-  }, []);
-
   return (
     <>
       <HeroVideo />
