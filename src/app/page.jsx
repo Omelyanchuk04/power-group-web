81f54a774c939160770daeae2ae90f3d11a335fb
Omelianchuk Oleksandr <89081142+Omelyanchuk04@users.noreply.github.com>
2026-04-15 00:13:39 +0300

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
 import About from "@/components/sections/About/About";
 import Services from "@/components/sections/Services/Services";
+import Process from "@/components/sections/Process/Process";
 
 export default function Home() {
   // 🚨 ЖУЧОК ДЛЯ ПОШУКУ СКРОЛУ
@@ -22,6 +23,7 @@ export default function Home() {
       <HeroVideo />
       <About />
       <Services />
+      <Process />
     </>
   );
 }
