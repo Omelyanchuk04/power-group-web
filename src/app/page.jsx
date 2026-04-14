a269cec51f117f12afc832bb9e4d8edd36ae68b1
Omelianchuk Oleksandr <89081142+Omelyanchuk04@users.noreply.github.com>
2026-04-14 22:47:12 +0300

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,32 +1,8 @@
-// import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
-// import About from "@/components/sections/About/About";
-// import Services from "@/components/sections/Services/Services";
-// import Projects from "@/components/sections/Projects/Projects";
-// import Process from "@/components/sections/Process/Process";
-// import Contacts from "@/components/sections/Contacts/Contacts";
-
-// export default function Home() {
-//   return (
-//     // 🔥 Використовуємо фрагмент <></>, щоб не дублювати <main>
-//     <>
-//       <HeroVideo />
-//       <About />
-//       <Services />
-//       <Projects />
-//       <Process />
-//       {/* <Contacts /> */}
-//     </>
-//   );
-// }
-
 "use client";
 
 import { useEffect } from "react";
 import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
 import About from "@/components/sections/About/About";
-import Services from "@/components/sections/Services/Services";
-import Projects from "@/components/sections/Projects/Projects";
-import Process from "@/components/sections/Process/Process";
 
 export default function Home() {
   // 🚨 ЖУЧОК ДЛЯ ПОШУКУ СКРОЛУ
@@ -44,9 +20,6 @@ export default function Home() {
     <>
       <HeroVideo />
       <About />
-      {/* <Services />
-      <Projects />
-      <Process /> */}
     </>
   );
 }
