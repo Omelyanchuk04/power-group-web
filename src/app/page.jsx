68c38a1242e134108367e0bc2740e5ca0d9b55d4
Omelianchuk Oleksandr <89081142+Omelyanchuk04@users.noreply.github.com>
2026-04-14 22:41:40 +0300

    fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,49 +1,52 @@
+// import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
+// import About from "@/components/sections/About/About";
+// import Services from "@/components/sections/Services/Services";
+// import Projects from "@/components/sections/Projects/Projects";
+// import Process from "@/components/sections/Process/Process";
+// import Contacts from "@/components/sections/Contacts/Contacts";
+
+// export default function Home() {
+//   return (
+//     // 🔥 Використовуємо фрагмент <></>, щоб не дублювати <main>
+//     <>
+//       <HeroVideo />
+//       <About />
+//       <Services />
+//       <Projects />
+//       <Process />
+//       {/* <Contacts /> */}
+//     </>
+//   );
+// }
+
 "use client";
 
+import { useEffect } from "react";
 import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
 import About from "@/components/sections/About/About";
 import Services from "@/components/sections/Services/Services";
 import Projects from "@/components/sections/Projects/Projects";
 import Process from "@/components/sections/Process/Process";
-import Contacts from "@/components/sections/Contacts/Contacts";
 
 export default function Home() {
+  // 🚨 ЖУЧОК ДЛЯ ПОШУКУ СКРОЛУ
+  useEffect(() => {
+    const findScroller = (e) => {
+      console.log("🔥 СКРОЛИТЬСЯ ЕЛЕМЕНТ:", e.target);
+    };
+
+    // Трекаємо всі скроли на сторінці
+    window.addEventListener("scroll", findScroller, true);
+    return () => window.removeEventListener("scroll", findScroller, true);
+  }, []);
+
   return (
-    // 🔥 Використовуємо фрагмент <></>, щоб не дублювати <main>
     <>
       <HeroVideo />
       <About />
       <Services />
       <Projects />
       <Process />
-      {/* <Contacts /> */}
     </>
   );
 }
-
-// "use client";
-
-// import { useEffect } from "react";
-// import HeroVideo from "@/components/sections/HeroVideo/HeroVideo";
-// import About from "@/components/sections/About/About";
-
-// export default function Home() {
-
-//   // 🚨 ЖУЧОК ДЛЯ ПОШУКУ СКРОЛУ
-//   useEffect(() => {
-//     const findScroller = (e) => {
-//       console.log("🔥 СКРОЛИТЬСЯ ЕЛЕМЕНТ:", e.target);
-//     };
-
-//     // Трекаємо всі скроли на сторінці
-//     window.addEventListener("scroll", findScroller, true);
-//     return () => window.removeEventListener("scroll", findScroller, true);
-//   }, []);
-
-//   return (
-//     <>
-//       <HeroVideo />
-//       <About />
-//     </>
-//   );
-// }
